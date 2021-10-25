e5f5db936a2e9b24e72707f8110180c1d0d5ae3f
mzeltsan <80985639+mzeltsan@users.noreply.github.com>
2021-10-25 13:50:19 -0400

    Create getUserByUserId.js

---
+++ request-response/dummy-backend/response/getUserByUserId.js
@@ -0,0 +1,56 @@
+module.exports = {
+    "ediMemberships": [],
+    "ediUserRoleMaps": [
+        {
+            "accessActiveFlag": 1,
+            "id": {
+                "facilityId": "MHS",
+                "roleId": "Admin",
+                "serviceAreaId": "MMC",
+                "userId": "mzeltsan"
+            }
+        },
+        {
+            "accessActiveFlag": 1,
+            "id": {
+                "facilityId": "MHS",
+                "roleId": "remits",
+                "serviceAreaId": "MMC",
+                "userId": "mzeltsan"
+            }
+        },
+        {
+            "accessActiveFlag": 1,
+            "id": {
+                "facilityId": "MHS",
+                "roleId": "Admin",
+                "serviceAreaId": "WPH",
+                "userId": "mzeltsan"
+            }
+        },
+        {
+            "accessActiveFlag": 1,
+            "id": {
+                "facilityId": "MHS",
+                "roleId": "Admin",
+                "serviceAreaId": "NYA",
+                "userId": "mzeltsan"
+            }
+        }
+    ],
+    "name": "mzeltsan",
+    "restApiKey": null,
+    "roleToString": "Admin:remits:Admin:Admin:",
+    "userActiveFlag": 1,
+    "userAddress1": " ",
+    "userAddress2": " ",
+    "userCity": " ",
+    "userEmail": "mzeltsan@montefiore.org",
+    "userFirstName": "micahel",
+    "userId": "mzeltsan",
+    "userLastName": "zeltsan",
+    "userPhone": " ",
+    "userPhone2": null,
+    "userState": " ",
+    "userZip": " "
+}
