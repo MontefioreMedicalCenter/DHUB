d1092b429540176bd16f26ad56dd8eb091db93bc
jiawumontefiore <78234703+jiawumontefiore@users.noreply.github.com>
2021-11-17 16:00:43 -0500

    Create runReport.js

---
+++ request-response/dummy-backend/response/runReport.js
@@ -0,0 +1,3 @@
+{
+    "999": "999 Envelope: Interchange Sender: INSTAMED, Receiver: 10146, Functional Group Sender: INSTAMED, Receiver: 10146\r\nThis 999 is acknowledging 'Health Care Claim (837)' with Function Group Control Number '217191'\r\n-----------------------------------------------------------------\r\nOverall Status: Accepted\r\nNumber of Transaction Sets Included: 30, Received: 30, Accepted: 30\r\n"
+}
