c3f5bab9ff3a7f56b7eb36d100d376680d82f287
jiawumontefiore <78234703+jiawumontefiore@users.noreply.github.com>
2021-11-16 20:16:30 -0500

    Create explainPayload.js

---
+++ request-response/dummy-backend/response/explainPayload.js
@@ -0,0 +1,5 @@
+999 Envelope: Interchange Sender: INSTAMED, Receiver: 10146, Functional Group Sender: INSTAMED, Receiver: 10146
+This 999 is acknowledging 'Health Care Claim (837)' with Function Group Control Number '217191'
+-----------------------------------------------------------------
+Overall Status: Accepted
+Number of Transaction Sets Included: 30, Received: 30, Accepted: 30
