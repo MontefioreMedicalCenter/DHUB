0abfec23d217e15cb3c0804c4ebb042c651216de
jiawumontefiore <78234703+jiawumontefiore@users.noreply.github.com>
2021-11-30 17:40:02 -0500

    Create getRemoteDirListing.js

---
+++ request-response/dummy-backend/response/getRemoteDirListing.js
@@ -0,0 +1,52 @@
+[
+    {
+        "adjustedFileDate": null,
+        "ageInDays": 99999999,
+        "fileDate": null,
+        "fileName": "reconftp@recon:/hipaa/reconftp/ICAN_PROD/Processed/999/MMC-Eagle",
+        "fileSize": 0,
+        "isDir": true,
+        "rawDirListEntry": "Success",
+        "timeOffset": 0
+    },
+    {
+        "adjustedFileDate": "2021-11-30 22:36:00",
+        "ageInDays": 1,
+        "fileDate": "2021-11-30 22:36:00",
+        "fileName": "apache license 2.0.txt",
+        "fileSize": 11358,
+        "isDir": false,
+        "rawDirListEntry": "-rw-r-----    1 reconftp staff         11358 Nov 30 17:36 apache license 2.0.txt",
+        "timeOffset": 0
+    },
+    {
+        "adjustedFileDate": "2021-11-30 22:35:00",
+        "ageInDays": 1,
+        "fileDate": "2021-11-30 22:35:00",
+        "fileName": "archive",
+        "fileSize": 256,
+        "isDir": true,
+        "rawDirListEntry": "drwxr-x---    2 reconftp staff           256 Nov 30 17:35 archive",
+        "timeOffset": 0
+    },
+    {
+        "adjustedFileDate": "2021-11-30 22:35:00",
+        "ageInDays": 1,
+        "fileDate": "2021-11-30 22:35:00",
+        "fileName": "bk20211130",
+        "fileSize": 256,
+        "isDir": true,
+        "rawDirListEntry": "drwxr-x---    2 reconftp staff           256 Nov 30 17:35 bk20211130",
+        "timeOffset": 0
+    },
+    {
+        "adjustedFileDate": "2021-11-30 22:35:00",
+        "ageInDays": 1,
+        "fileDate": "2021-11-30 22:35:00",
+        "fileName": "temp1",
+        "fileSize": 256,
+        "isDir": true,
+        "rawDirListEntry": "drwxr-x---    2 reconftp staff           256 Nov 30 17:35 temp1",
+        "timeOffset": 0
+    }
+]
