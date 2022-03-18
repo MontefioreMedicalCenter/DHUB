c47786704d1290696d7fa6719a82971ee3b5753e
mmishra1008 <mmishra@montefiore.org>
2022-03-18 10:40:59 -0400

    checked

--- src/container/views/itemRenderers/CustomToolTipRender.js
+++ src/container/views/itemRenderers/CustomToolTipRender.js
@@ -37,70 +37,72 @@ class CustomToolTipRender extends React.Component {
 	handleOpenPopup = event => {
 		// var ppp = this.props
 		// console.log(this.props.column.dataField)
-		if (this.props.column.dataField === 'eagleClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().eagleClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'idxmotClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().idxmotClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'idxmocClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().idxmocClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'hhClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().hhClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'hb_epicClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().hb_epicClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'pb_epicClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().pb_epicClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'hh_epicClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().hh_epicClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'satpClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().satpClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'dosaClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().dosaClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'cercClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().cercClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'ucpClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().ucpClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'meditechClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().meditechClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'zotecClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().zotecClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'ucpClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().ucpClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'mckessonClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().mckessonClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'mckesson2ClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().mckesson2ClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'caduceusClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().caduceusClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'obgynClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().obgynClaimPaymentStr })
-		}
-		if (this.props.column.dataField === 'otherClaimPaymentCount') {
-			return this.setState({ textData: this.props.cell.rowInfo.getData().otherClaimPaymentStr })
-		}
-		return this.setState({textData: '*'})
+		return this.setState({textData: this.props.cell.rowInfo.getData()[this.props.column.dataField.replace('Count', 'Str')]})
+		// if (this.props.column.dataField === 'eagleClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().eagleClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'idxmotClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().idxmotClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'idxmocClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().idxmocClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'hhClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().hhClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'hb_epicClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().hb_epicClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'pb_epicClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().pb_epicClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'hh_epicClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().hh_epicClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'satpClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().satpClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'dosaClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().dosaClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'cercClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().cercClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'ucpClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().ucpClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'meditechClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().meditechClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'zotecClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().zotecClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'ucpClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().ucpClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'mckessonClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().mckessonClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'mckesson2ClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().mckesson2ClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'caduceusClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().caduceusClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'obgynClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().obgynClaimPaymentStr })
+		// }
+		// if (this.props.column.dataField === 'otherClaimPaymentCount') {
+		// 	return this.setState({ textData: this.props.cell.rowInfo.getData().otherClaimPaymentStr })
+		// }
+		// return this.setState({textData: '*'})
 	}
 
 	render() {
 		return (
 			<div onMouseEnter={() => this.setState({ visible: 'visible' })} onMouseLeave={() => this.setState({ visible: 'hidden' })} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', columnGap: '10px', margin: '3px' }}>
-				{this.props.column.dataField === 'eagleClaimPaymentCount' ? this.props.row.getData().eagleClaimPaymentCount : null}
+				{this.props.column.dataField.includes('ClaimPaymentCount')? this.props.row.getData()[this.props.column.dataField] : null}
+				{/* {this.props.column.dataField === 'eagleClaimPaymentCount' ? this.props.row.getData().eagleClaimPaymentCount : null}
 				{this.props.column.dataField === 'idxmotClaimPaymentCount' ? this.props.row.getData().idxmotClaimPaymentCount : null}
 				{this.props.column.dataField === 'hhClaimPaymentCount' ? this.props.row.getData().hhClaimPaymentCount : null}
 				{this.props.column.dataField === 'hb_epicClaimPaymentCount' ? this.props.row.getData().hb_epicClaimPaymentCount : null}
@@ -110,6 +112,9 @@ class CustomToolTipRender extends React.Component {
 				{this.props.column.dataField === 'dosaClaimPaymentCount' ? this.props.row.getData().dosaClaimPaymentCount : null}
 				{this.props.column.dataField === 'hh_epicClaimPaymentCount' ? this.props.row.getData().hh_epicClaimPaymentCount : null}
 				{this.props.column.dataField === 'cercClaimPaymentCount' ? this.props.row.getData().cercClaimPaymentCount : null}
+				{this.props.column.dataField === 'meac_hbClaimPaymentCount' ? this.props.row.getData().meac_hbClaimPaymentCount : null}
+				{this.props.column.dataField === 'meac_pbClaimPaymentCount' ? this.props.row.getData().meac_pbClaimPaymentCount : null} */}
+
 				<HtmlTooltip
 					title={
 						<div>
